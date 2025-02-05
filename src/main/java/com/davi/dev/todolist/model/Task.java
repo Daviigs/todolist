a6e15b3536d0f2453f36767c29686d00a2d60da1
Davi Miquéias <miqueiasdavi8@gmail.com>
2025-02-05 16:50:58 -0300

    Model Task created

---
+++ src/main/java/com/davi/dev/todolist/model/Task.java
@@ -0,0 +1,33 @@
+package com.davi.dev.todolist.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime dateStart;
+    private LocalDateTime dateEnd;
+    private String priority;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private UUID idUser;
+}
