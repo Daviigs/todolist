ea3e7718432eacfe62320da098541570f707f89d
Davi Miquéias <miqueiasdavi8@gmail.com>
2025-02-07 13:38:53 -0300

    Data validation

--- src/main/java/com/davi/dev/todolist/controllers/TaskController.java
+++ src/main/java/com/davi/dev/todolist/controllers/TaskController.java
@@ -5,11 +5,14 @@ import com.davi.dev.todolist.repository.ITaskRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -20,10 +23,23 @@ public class TaskController {
     private ITaskRepository iTaskRepository;
 
     @PostMapping("/")
-    public Task create(@RequestBody Task task, HttpServletRequest request) {
-        System.out.println(" Chegou no controller, " + request.getAttribute("idUser"));
+    public ResponseEntity create(@RequestBody Task task, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         task.setIdUser((UUID) idUser);
-        return this.iTaskRepository.save(task);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(task.getDateStart()) || currentDate.isAfter(task.getDateEnd())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de inécio/término deve ser maior que a data atual");
+        }
+
+        if (task.getDateStart().isAfter(task.getDateEnd())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data término não pode ser antes da data de início");
+        }
+        if (task.getDateStart().equals(task.getDateEnd())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de início não pode ser igual a de término");
+        }
+
+        var taskModel = this.iTaskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.OK).body(taskModel);
     }
 }
